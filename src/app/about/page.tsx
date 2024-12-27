7c228aa9ae79312b6f016e424c1bc33d92065114
soe-un <runterf@gmail.com>
2024-12-27 23:20:52 +0900

    about me

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,8 +1,14 @@
 export default function About() {
   return (
-    <main>
-      <div className="flex gap-4 items-center flex-col sm:flex-row">
-        <h1>저는 이런 사람이에요</h1>
+    <main className="mt-[25%]">
+      <div className="gap-4 items-center">
+        <h1 className="text-6xl">About Me</h1>
+        <div className="mt-10 text-2xl">
+          단순한 개발을 넘어 업무를 <strong>효율적</strong>으로 개선하는 일을
+          생각하고, <br />
+          <strong>비즈니스에 기여</strong>할 수 있는 부분을 찾기 위해
+          노력합니다.
+        </div>
       </div>
     </main>
   );
